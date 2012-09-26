94f521ba35ecedd64bb12ac618630e74de30f640
Julio Saito <saitodisse@gmail.com>
2012-09-26 13:12:59 -0300

    Initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyMusicDatabase::Application.config.secret_token = '4abd913040d6d0e6929a2251ed1d3c17d85c3d2e744a1c42f7b5e84a888d87a511d60c4ef289469c3640096d1e9b805fd49eaa05a70df2f53cdc4c44489286fd'
